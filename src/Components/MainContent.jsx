e0b3cf7329444f34be25a79874a60adc7fb26146
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-04-25 21:52:09 +0400

    feat: uploading progress...

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
+import couple from "../assets/Images/trendy-fashionable.jpg";
+
 const MainContent = () => {
   return (
     <main>
       <section className="bg-[#F2F0F1] flex justify-center align-center">
-        <div className="flex">
-          <div>
-            <h1 className="font-extrabold text-6xl text-wrap max-w-[577px] integral-cf">
+        <div className="flex justify-center align-center flex-col md:flex-row ">
+          <div className="sm:mt-16 md:mt-20 lg:mt-28 max-w-1/2">
+            <h1 className="font-extrabold text-6xl text-wrap  integral-cf sm:mb-4 md:mb-6 lg:mb-8">
               FIND CLOTHES THAT MATCHES YOUR STYLE
             </h1>
+            <p className="satoshi text-[#00000099] ">
+              Browse through our diverse range of meticulously crafted garments,
+              designed to bring out your individuality and cater to your sense
+              of style.
+            </p>
           </div>
-          <div>Right</div>
+          <div className="w-1/2 overflow-hidden">
+            <img
+              src={couple}
+              className="couple object-top max-h-[660px]"
+              alt="Couple"
+            />
+          </div>
         </div>
       </section>
     </main>
---
+++ src/Components/Ui/Button.jsx
@@ -0,0 +1,11 @@
+const Button = ({ children }) => {
+  return (
+    <button
+      type="button"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
